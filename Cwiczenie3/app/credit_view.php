2e219407ab23b09abd347d4e61cd4ab57bf7233b
Kanareo <72729896+Kanareo@users.noreply.github.com>
2021-03-20 16:31:51 +0100

    Copied Cwiczenie2 to new folder

---
+++ Cwiczenie3/app/credit_view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+</head>
+<body>
+    
+<div style="width:90%; margin: 2em auto;">
+    <a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+</div>
+
+<form action="<?php print(_APP_URL);?>/app/credit_calc.php" method="post">
+	<label for="id_x">Kwota kredytu: </label>
+	<input id="id_x" type="text" name="x" value="<?php if (isset($x)) print($x); ?>" /><br />
+	<label for="id_y">Ilość lat: </label>
+	<input id="id_y" type="text" name="y" value="<?php if (isset($y)) print($y); ?>" /><br />
+	<label for="id_y">Oprocentowanie roczne: </label>
+	<input id="id_y" type="text" name="p" value="<?php if (isset($p)) print($p); ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+if (isset($messages)) {
+	if (count($messages) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Kwota miesięczna do zapłaty: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
